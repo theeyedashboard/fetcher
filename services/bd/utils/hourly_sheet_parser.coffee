f010972fd0be18745d57c521c26bc8a0acd1b093
Cedric Villa <cedric.villa@gmail.com>
2016-10-30 19:50:52 +0100

    add basic parsing (articles unit names&prices)

--- services/bd/utils/hourly_sheet_parser.coffee
+++ services/bd/utils/hourly_sheet_parser.coffee
@@ -1,13 +1,66 @@
 moment = require('moment')
 DatesFixer = require('./dates_fixer')
 
+ARTICLES_INDEXES_ROW    = 8
+ARTICLES_LABELS_ROW     = 9
+ARTICLES_UNIT_PRICE_ROW = 10
+
 class HourlySheetParser
 
   parse: (records, folder, filename) ->
     results = {}
+    results['folder'] = folder
+    results['hours'] = @parse_hours(records)
     try
       return { status: 0, data: results }
     catch error
       return { status: -1, error: error }
+
+  parse_hours: (records) =>
+    hours = {}
+    start_counting = false
+    hour_index = 0
+    for record, index in records
+      if record[0] and !start_counting
+        start_counting = true
+      if start_counting
+        if record[0] and record[0].indexOf('TOTAL') == -1
+          hours[hour_index.toString()] = @parse_hour(records, index)
+          hour_index++
+    return hours
+
+  parse_hour: (records, index) =>
+    record = records[index]
+    return {
+      name:         record[0],
+      total:        @parse_int(record[1]),
+      revenues:     @parse_int(record[2]),
+      annulations:  @parse_int(record[3]),
+      offerts:      @parse_int(record[4]),
+      articles:     @parse_articles_for_index(records, index)
+    }
+
+  parse_int: (string) =>
+    if string
+      return parseInt(
+        string.replace( /^\D+/g, '')
+      )
+    else
+      return 0
+
+  parse_articles_for_index: (records, index) =>
+    articles = []
+    # parse column with an article index
+    for column, column_index in records[index]
+      article_index = records[ARTICLES_INDEXES_ROW][column_index]
+      article_name = records[ARTICLES_LABELS_ROW][column_index]
+      unit_price = @parse_int(records[ARTICLES_UNIT_PRICE_ROW][column_index])
+      if article_index && article_name
+        articles.push {
+          title: article_name,
+          unit_price: unit_price
+        }
+    return articles
+
 
 module.exports = HourlySheetParser
--- services/bd/utils/hourly_sheet_parser.spec.js
+++ services/bd/utils/hourly_sheet_parser.spec.js
@@ -9,11 +9,122 @@ const hourly_data_sheet = require('./test_data/hourly_sheet.json')
 describe ('HourlySheetParser', () => {
   const hourlySheetParser = new HourlySheetParser()
   let results = hourlySheetParser.parse(hourly_data_sheet, "Le Cozy/Test - Horaire - Vestiaire", "VentesHoraire_BV_2016_septembre_LE COZY.xls")
-
   it('should return status 0', () => {
     expect(
       results['status']
-    ).toEqual(0)    
+    ).toEqual(0)
+  })
+
+  it('should return folder', () => {
+    expect(
+      results['data']['folder']
+    ).toEqual('Le Cozy/Test - Horaire - Vestiaire')
+  })
+
+  let hours = results['data']['hours']
+
+  describe('total', () => {
+    it('should return 123 for hour 9', () => {
+      expect(
+        hours['9']['total']
+      ).toEqual(123)
+    })
+  })
+
+  describe('revenues', () => {
+    it('should return 276 for hour 9', () => {
+      expect(
+        hours['9']['revenues']
+      ).toEqual(276)
+    })
+    it('should return 0 for hour 6', () => {
+      expect(
+        hours['6']['revenues']
+      ).toEqual(0)
+    })
+  })
+
+  describe('revenues', () => {
+    it('should return 276 for hour 9', () => {
+      expect(
+        hours['9']['revenues']
+      ).toEqual(276)
+    })
+    it('should return 0 for hour 20', () => {
+      expect(
+        hours['20']['revenues']
+      ).toEqual(0)
+    })
+  })
+
+
+  describe('annulations', () => {
+    it('should return 0 for hour 9', () => {
+      expect(
+        hours['9']['annulations']
+      ).toEqual(0)
+    })
+    it('should return 7 for hour 18', () => {
+      expect(
+        hours['18']['annulations']
+      ).toEqual(7)
+    })
+  })
+
+  describe('offerts', () => {
+    it('should return 4 for hour 8', () => {
+      expect(
+        hours['8']['offerts']
+      ).toEqual(4)
+    })
+    it('should return 0 for hour 10', () => {
+      expect(
+        hours['10']['offerts']
+      ).toEqual(0)
+    })
+  })
+
+  describe('articles', () => {
+    it('should return expected {} for hour 9', () => {
+      let expected_articles = [
+        {
+          title: "1 VETEMENT",
+          unit_price: 2,
+        },
+        {
+          title: "2 VETEMENTS",
+          unit_price: 4
+        },
+        {
+          title: "1 PETIT SAC",
+          unit_price: 2
+        },
+        {
+          title: "1 GROS SAC",
+          unit_price: 4
+        },
+        {
+          title: "1 CASQUE",
+          unit_price: 2
+        },
+        {
+          title: "CIGARETTE",
+          unit_price: 10
+        },
+        {
+          title: "CAFE",
+          unit_price: 2
+        },
+      ]
+      expect(
+        hours['10']['articles']
+      ).toEqual(expected_articles)
+    })
+    it('should return 0 for hour 10', () => {
+      expect(
+        hours['10']['offerts']
+      ).toEqual(0)
+    })
   })
 
 
